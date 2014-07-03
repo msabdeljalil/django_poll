8848d8034d837e9726a164267d7b17ab3c706e45
msabdeljalil <msabdeljalil@gmail.com>
2014-07-03 16:12:16 -0500

    Adding related (choice) objects to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
+
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 3
 
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
 		("Ony field to fill...", { 'fields': ['question'] } ),
 		('Date information', { 'fields': ['pub_date'], 'classes': ['collapse'] } ),
 	]
-	
-	# fields = ['pub_date', 'question']
+	inlines = [ChoiceInLine]
 
 admin.site.register(Poll, PollAdmin)
