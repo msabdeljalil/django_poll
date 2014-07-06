8db2814295d538984dd383c8213224ec454ede8c
msabdeljalil <msabdeljalil@gmail.com>
2014-07-06 12:26:07 -0500

    Testing in Django

--- polls/views.py
+++ polls/views.py
@@ -27,24 +27,39 @@ from polls.models import Choice, Poll
 
 # def results(request, poll_id):
 #   # return HttpResponse("You're looking at the results of poll %s." % poll_id)
-# 	poll = get_object_or_404(Poll, pk=poll_id)
-# 	return render( request, 'polls/results.html', {'poll':poll} )
+#   poll = get_object_or_404(Poll, pk=poll_id)
+#   return render( request, 'polls/results.html', {'poll':poll} )
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_poll_list'
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_poll_list'
 
-	def get_queryset(self):
-		"""Return the last five published polls"""
-		return Poll.objects.order_by('-pub_date')[:5]
+  def get_queryset(self):
+    # """Return the last five published polls"""
+    # return Poll.objects.order_by('-pub_date')[:5]
+    # ------------------------------------------------------------1
+    # '''
+    # Return the last five published polls (not including those 
+    # published in the future)
+    # '''
+    # return Poll.objects.filter(
+    #   pub_date__lte=timezone.now()
+    # ).order_by('-pub_date')[:5]
+    # ------------------------------------------------------------2
+    """
+    Excludes any polls that aren't published yet.
+    """
+    return Poll.objects.filter(pub_date__lte=timezone.now())
+
 
 class DetailView(generic.DetailView):
-	model = Poll
-	template_name = 'polls/detail.html'
+  model = Poll
+  template_name = 'polls/detail.html'
+  
 
 class ResultsView(generic.DetailView):
-	model = Poll
-	template_name = 'polls/results.html'
+  model = Poll
+  template_name = 'polls/results.html'
 
 def vote(request, poll_id):
   p = get_object_or_404(Poll, pk=poll_id)
